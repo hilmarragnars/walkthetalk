99fab42344e2c31b1bb9a32f6bf757a91b60332e
Hilmar Ragnarsson <hilmarragnars@gmail.com>
2019-09-04 20:23:12 +0200

    Change the name of the todo app

--- walkthetalk/settings.py
+++ walkthetalk/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'backend',
+    'todo',
     'frontend',
     'accounts',
     'rest_framework',
--- walkthetalk/urls.py
+++ walkthetalk/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import include
 from rest_framework import routers
 from django.contrib import admin
 from django.urls import path
-from backend import views as backend_views
+from todo import views as todo_views
 from frontend import urls as frontend_urls
 
 router = routers.DefaultRouter()
-router.register(r'todos', backend_views.TodoView, 'todos')
+router.register(r'todos', todo_views.TodoView, 'todos')
 
 urlpatterns = [
     path('', include(frontend_urls)),
