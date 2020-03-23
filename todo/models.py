da114ebddaf364cec9bfface3d712dbd4362441a
Hilmar Ragnarsson <hilmarragnars@gmail.com>
2020-03-23 22:07:52 +0100

    Test migrations

--- todo/models.py
+++ todo/models.py
@@ -10,3 +10,32 @@ class Todo(models.Model):
 
     def _str_(self):
         return self.title
+
+
+class Card(models.Model):
+    class Suit(models.IntegerChoices):
+        DIAMOND = 1
+        SPADE = 2
+        HEART = 3
+        CLUB = 4
+
+    class Ranking(models.IntegerChoices):
+        TWO = 1
+        THREE = 2
+        FOUR = 3
+        FIVE = 4
+        SIX = 5
+        SEVEN = 6
+        EIGHT = 7
+        NINE = 8
+        TEN = 9
+        JACK = 10
+        QUEEN = 11
+        KING = 12
+        ACE = 13
+
+    ranking = models.IntegerField(choices=Ranking.choices)
+    suit = models.IntegerField(choices=Suit.choices)
+
+    def _str_(self):
+        return self.ranking + ' of ' + self.suit
